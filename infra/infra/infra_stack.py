8738a20ebe91712a280a0398413aab39107cdaeb
tomokiikawa <ikawatomoki91011@gmail.com>
2023-04-20 03:44:48 +0900

    Modify infra container

--- infra/infra/infra_stack.py
+++ infra/infra/infra_stack.py
@@ -1,8 +1,11 @@
+import os
+
 from aws_cdk import (
     Stack,
     aws_ec2 as ec2,
     aws_ecs as ecs,
     aws_ecs_patterns as ecs_patterns,
+    aws_ecr_assets as ecr_assets,
 )
 from constructs import Construct
 
@@ -15,11 +18,15 @@ class InfraStack(Stack):
 
         cluster = ecs.Cluster(self, "Cluster", vpc=vpc)
 
+        asset = ecr_assets.DockerImageAsset(self, "petgram",
+            directory=os.path.join(os.getcwd(), "../app")
+        )
+
         ecs_patterns.ApplicationLoadBalancedFargateService(self, "Service",
             cluster=cluster,
             cpu=512,
             desired_count=1,
             task_image_options=ecs_patterns.ApplicationLoadBalancedTaskImageOptions(
-                image=ecs.ContainerImage.from_registry("amazon/amazon-ecs-sample")),
+                image=ecs.ContainerImage.from_docker_image_asset(asset)),
             memory_limit_mib=2048,
             public_load_balancer=True)
