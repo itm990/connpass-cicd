1d3a28b1f514e4642e7f226b7cd90a4782897f91
tomokiikawa <ikawatomoki91011@gmail.com>
2023-04-20 04:15:35 +0900

    Modify app name

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -158,7 +158,7 @@ function App() {
           <nav className="navbar">
             <div className="navbar__logo">
               <ion-icon name="paw-outline"></ion-icon>
-              <p className="navbar__text-logo">更新グラム</p>
+              <p className="navbar__text-logo">ペットグラム</p>
             </div>
 
             <div className="navbar__search-bar">
